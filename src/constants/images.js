1e08ff2b6be757b75f0836ce962352831952b8d9
Sudarsh <55168611+Sudarsh1010@users.noreply.github.com>
2022-03-30 16:36:47 +0530

    constants

---
+++ src/constants/images.js
@@ -0,0 +1,15 @@
+// Importing Profile Pictures from Assets
+import speaker1 from '../assets/profile/julian.png';
+import speaker2 from '../assets/profile/julian2.png';
+import speaker3 from '../assets/profile/julian3.png';
+
+// Importing Logo from Assets
+import logo from '../assets/logo/logo.png';
+
+export default {
+  speaker1,
+  speaker2,
+  speaker3,
+
+  logo
+}
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+// exporting all images as constant
+export { default as images } from './images';
