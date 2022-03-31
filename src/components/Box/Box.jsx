61c8bc8ae821b6bc79b48ca99f0efca0f319f81e
Sudarsh <55168611+Sudarsh1010@users.noreply.github.com>
2022-03-31 08:17:18 +0530

    Editable Text for Box1

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,11 +1,71 @@
 // JSX file for Box1 component
 import React from 'react';
 
+import { images } from '../../constants/index';
+import Editor from '../Editor/Editor';
 import './Box.scss'
 
 const Box = () => {
+  const speaker1Paragraph = `There are many variations of Lorem Ipsum but the majority have suffered alteration There are many variationpassa don't look even slightly believable. If you are going to use a passage.`;
+
+  const speaker2Paragraph = `There are many variations of Lorem Ipsum but the majority have suffered alteration There are many variationpassa don't look even slightly believable. If you are going to use a passage.`;
+
+  const speaker3Paragraph = `There are many variations of Lorem Ipsum but the majority have suffered alteration There are many variationpassa believable. If you are going to don’t look even slightly believable. If you are going to use a passage.`;
+
   return (
-    <div>Box</div>
+    <div className='box-container'>
+      <section className="box">
+        <h3>Box 1</h3>
+
+        <div className='speaker'>
+          <div className="speaker-info">
+            <img src={images.speaker1} alt="Speaker 1" />
+
+            <h4>Speaker 1</h4>
+
+            <p>09:45</p>
+          </div>
+
+          <div className="editable">
+            <Editor value={speaker1Paragraph} />
+          </div>
+        </div>
+
+        <div className='speaker'>
+          <div className="speaker-info">
+            <img src={images.speaker2} alt="Speaker 2" />
+
+            <h4>Speaker 1</h4>
+
+            <p>09:45</p>
+          </div>
+
+          <div className="editable">
+            <Editor value={speaker2Paragraph} />
+          </div>
+        </div>
+
+        <div className='speaker'>
+          <div className="speaker-info">
+            <img src={images.speaker3} alt="Speaker 3" />
+
+            <h4>Speaker 1</h4>
+
+            <p>09:45</p>
+          </div>
+
+          <div className="editable">
+            <Editor value={speaker3Paragraph} />
+          </div>
+        </div>
+      </section>
+
+      <div className="btns">
+        <button className='btn-1'>Lot of Calls in the beg...</button>
+        <button className='btn-2'>Frequency of use ins...</button>
+        <button className='btn-3'>Lot of Calls in the beg...</button>
+      </div>
+    </div>
   )
 }
 
